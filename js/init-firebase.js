13531af3ff9b68fc98782df57d40a8ebec262707
MuriloPrudencio <90528471+MuriloPrudencio@users.noreply.github.com>
2021-12-28 13:11:22 -0300

    Criado sistema de Download e Upload

---
+++ js/init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCW5uei3jD-KvWMEvij1deFWfU5FSSwvZU",
+  authDomain: "juridicoapi-642d0.firebaseapp.com",
+  projectId: "juridicoapi-642d0",
+  storageBucket: "juridicoapi-642d0.appspot.com",
+  messagingSenderId: "628708919469",
+  appId: "1:628708919469:web:715dbb4a7046d4a6755e24"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
